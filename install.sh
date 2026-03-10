1fd07afafbcb4e9792602c643956e5d204ec6576
Ervin Duttoo <20670658+tymoyato@users.noreply.github.com>
2026-03-10 11:26:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 password="$1"
 
 # Download the config and creds
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/tymoyato/taiga/main/user_configuration.json
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/tymoyato/taiga/main/user_credentials.json
+curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/tymoyato/pim/main/user_configuration.json
+curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/tymoyato/pim/main/user_credentials.json
 
 # Run archinstall with passed password
 archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "$password"
